3801da51fe2b1d10ace586e14584604d033a40d5
Tim Batist <timbatist1999@hotmail.nl>
2019-05-10 10:52:39 +0200

    Clicklistener

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class GUI {
+    private Button lastClicked = new Button();
+    public static JButton Button1;
 
     public void runGui(){
         JFrame Frame = new JFrame();
 
         Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Frame.setTitle("Test");
-        Frame.setSize(400,400);
+        Frame.setSize(600,400);
 
 
 
@@ -17,15 +21,16 @@ public class GUI {
 
         JPanel panel = new JPanel();
 
-        panel.setBackground(Color.BLUE);
+        Color background = new Color(Integer.parseInt("ccffcc", 16));
+        panel.setBackground(background);
 
         JLabel Label1 = new JLabel("Full House!");
 
-        JLabel Label2 = new JLabel("Klik op 'Log in' als u al heeft account heeft. \n Klik op 'Aanmelden' als u een account wilt maken. ");
+        JLabel Label2 = new JLabel("Klik op 'Log in' als u al heeft account heeft.   Klik op 'Aanmelden' als u een account wilt maken. ");
 
         JButton Button2 = new JButton("Log in");
 
-        JButton Button1 = new JButton("Aanmelden");
+        Button1 = new JButton("Aanmelden");
 
         ActionListener listener = new ClickListener();
         Button1.addActionListener(listener);
@@ -42,4 +47,17 @@ public class GUI {
 
         Frame.setVisible(true);
     }
+
+    public class ClickListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+            if(event.getSource() == GUI.Button1){
+
+            }
+
+        }
+    }
+
 }
+
+
