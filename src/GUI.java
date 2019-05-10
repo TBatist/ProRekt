e40b866110f28f8fd732f509811a81e0a084f93b
lisaf <lisafkerkhof@gmail.com>
2019-05-10 09:36:19 +0200

    Regels

--- src/GUI.java
+++ src/GUI.java
@@ -11,18 +11,20 @@ public class GUI {
 
         JPanel panel = new JPanel();
 
-        JLabel Label1 = new JLabel("tekst1");
+        JLabel Label1 = new JLabel("Full House!");
 
-        JLabel Label2 = new JLabel("tekst2");
+        JLabel Label2 = new JLabel("Klik op 'Log in' als u al heeft account heeft. \n Klik op 'Aanmelden' als u een account wilt maken.");
 
-        JLabel Label3 = new JLabel("tekst");
+        JButton Button2 = new JButton("Log in");
+
+        JButton Button1 = new JButton("Aanmelden");
 
 
         panel.add(Label1);
 
         panel.add(Label2);
 
-        panel.add(Label3);
+        panel.add(Button1);
 
         Frame.add(panel);
 
